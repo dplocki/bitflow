f52bd9c933efe9f4f495eb79b8c704998b4cdb85
dplocki <dawid.plocki@gmail.com>
2025-02-20 19:59:39 +0100

    feat: implement TCP connection and hex message sending

--- hello.go
+++ hello.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"encoding/hex"
+	"fmt"
+	"net"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	conn, err := net.Dial("tcp", "localhost:9092")
+	if err != nil {
+		fmt.Println("Error connecting to server:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to server.")
+
+	hexString := "000000230012674a4f74d28b00096b61666b612d636c69000a6b61666b612d636c6904302e3100"
+	message, err := hex.DecodeString(hexString)
+	if err != nil {
+		fmt.Println("Error decoding hex string:", err)
+		return
+	}
+
+	_, err = conn.Write(message)
+	if err != nil {
+		fmt.Println("Error sending message:", err)
+		return
+	}
+
+	buffer := make([]byte, 1024)
+	n, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return
+	}
+
+	fmt.Print("Server response: ", string(buffer[:n]))
 }
