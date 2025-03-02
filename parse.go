d222163123aadd1716fc71d7d4d8603fc59b68cb
dplocki <dawid.plocki@gmail.com>
2025-03-02 10:13:48 +0100

    feat: enhance message normalization and update example messages in config

--- parse.go
+++ parse.go
@@ -2,16 +2,31 @@ package main
 
 import (
 	"encoding/hex"
+    "strings"
 )
+
+func normalizeMessage(message string) string {
+    if strings.Contains(message, "|") {
+        parts := strings.Split(message, "|")
+        if len(parts) > 1 {
+            message = parts[1]
+        }
+    }
+
+    return strings.ReplaceAll(message, " ", "")
+}
 
 func ParsingMessages(messages []string) ([][]byte, error) {
     result := make([][]byte, len(messages))
+
     for index, message := range messages {
+        message = normalizeMessage(message)
         bytes, err := hex.DecodeString(message)
         if err != nil {
             return nil, err
         }
         result[index] = bytes
     }
+
     return result, nil
 }
